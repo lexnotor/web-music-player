a398c87c693577c63aa8adce3588bf6cb8ac5c6f
alexandre-notor <amanichambu48@gmail.com>
2022-08-29 11:25:57 +0200

    ajout du dossier redux

---
+++ src/components/Lexify/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+/**
+ * Composant destin à accueil l'application au complet
+ */
+
+class Lexify extends Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Lexify;
